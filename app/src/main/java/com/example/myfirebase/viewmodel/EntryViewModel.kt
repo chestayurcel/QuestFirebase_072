9352a58acf981f768d2ee4bb532483d77ea202da
Yurcell <chestayurcel@gmail.com>
2026-01-02 17:58:49 +0700

    Tambahkan EntryViewModel untuk menangani logika entri data siswa

---
+++ app/src/main/java/com/example/myfirebase/viewmodel/EntryViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.myfirebase.viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.myfirebase.modeldata.DetailSiswa
+import com.example.myfirebase.modeldata.UIStateSiswa
+import com.example.myfirebase.repositori.RepositorySiswa
+import com.example.myfirebase.modeldata.toDataSiswa
+import androidx.compose.runtime.*
+
+class EntryViewModel(private val repositorySiswa: RepositorySiswa) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean
+    {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput
+                (detailSiswa))
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            repositorySiswa.postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
+        }
+    }
+}
